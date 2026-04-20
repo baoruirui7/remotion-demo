24e37e1d1058a3e2e2ef90e78629ff3682089e83
baoruirui7 <baoruirui.7@bytedance.com>
2026-04-20 11:15:58 +0800

    feat: add FadeWrapper for smooth scene transitions

--- src/ByteDance/ByteDanceIntro.tsx
+++ src/ByteDance/ByteDanceIntro.tsx
@@ -1,4 +1,4 @@
-import { AbsoluteFill, Sequence } from "remotion";
+import { AbsoluteFill, interpolate, Sequence, useCurrentFrame, useVideoConfig } from "remotion";
 import { Scene1Logo } from "./Scene1Logo";
 import { Scene2Stats } from "./Scene2Stats";
 import { Scene3Products } from "./Scene3Products";
@@ -12,27 +12,62 @@ import { Scene5Outro } from "./Scene5Outro";
 // Scene4: 360-480  (12s - 16s)
 // Scene5: 480-600  (16s - 20s)
 
+const FADE_DURATION = 18; // frames for fade in / fade out
+
+const FadeWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const frame = useCurrentFrame();
+  const { durationInFrames } = useVideoConfig();
+
+  const fadeIn = interpolate(frame, [0, FADE_DURATION], [0, 1], {
+    extrapolateLeft: "clamp",
+    extrapolateRight: "clamp",
+  });
+  const fadeOut = interpolate(
+    frame,
+    [durationInFrames - FADE_DURATION, durationInFrames],
+    [1, 0],
+    { extrapolateLeft: "clamp", extrapolateRight: "clamp" }
+  );
+  const opacity = Math.min(fadeIn, fadeOut);
+
+  return (
+    <AbsoluteFill style={{ opacity }}>
+      {children}
+    </AbsoluteFill>
+  );
+};
+
 export const ByteDanceIntro: React.FC = () => {
   return (
     <AbsoluteFill style={{ background: "#000000" }}>
       <Sequence from={0} durationInFrames={120}>
-        <Scene1Logo />
+        <FadeWrapper>
+          <Scene1Logo />
+        </FadeWrapper>
       </Sequence>
 
       <Sequence from={120} durationInFrames={120}>
-        <Scene2Stats />
+        <FadeWrapper>
+          <Scene2Stats />
+        </FadeWrapper>
       </Sequence>
 
       <Sequence from={240} durationInFrames={120}>
-        <Scene3Products />
+        <FadeWrapper>
+          <Scene3Products />
+        </FadeWrapper>
       </Sequence>
 
       <Sequence from={360} durationInFrames={120}>
-        <Scene4Mission />
+        <FadeWrapper>
+          <Scene4Mission />
+        </FadeWrapper>
       </Sequence>
 
       <Sequence from={480} durationInFrames={120}>
-        <Scene5Outro />
+        <FadeWrapper>
+          <Scene5Outro />
+        </FadeWrapper>
       </Sequence>
     </AbsoluteFill>
   );
