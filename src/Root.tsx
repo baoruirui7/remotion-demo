736322a5681d34014197f9c4ed29d9a8c4c2ea36
baoruirui7 <baoruirui.7@bytedance.com>
2026-04-20 10:59:29 +0800

    fix: add missing src/ByteDance source files

--- src/Root.tsx
+++ src/Root.tsx
@@ -3,12 +3,9 @@ import { HelloWorld, myCompSchema } from "./HelloWorld";
 import { Logo, myCompSchema2 } from "./HelloWorld/Logo";
 import { ByteDanceIntro } from "./ByteDance/ByteDanceIntro";
 
-// Each <Composition> is an entry in the sidebar!
-
 export const RemotionRoot: React.FC = () => {
   return (
     <>
-      {/* ByteDance 20s Product Intro */}
       <Composition
         id="ByteDanceIntro"
         component={ByteDanceIntro}
@@ -17,18 +14,13 @@ export const RemotionRoot: React.FC = () => {
         width={1920}
         height={1080}
       />
-
       <Composition
-        // You can take the "id" to render a video:
-        // npx remotion render HelloWorld
         id="HelloWorld"
         component={HelloWorld}
         durationInFrames={150}
         fps={30}
         width={1920}
         height={1080}
-        // You can override these props for each render:
-        // https://www.remotion.dev/docs/parametrized-rendering
         schema={myCompSchema}
         defaultProps={{
           titleText: "Welcome to Remotion",
@@ -37,8 +29,19 @@ export const RemotionRoot: React.FC = () => {
           logoColor2: "#86A8E7",
         }}
       />
-
-      {/* Mount any React component to make it show up in the sidebar and work on it individually! */}
       <Composition
         id="OnlyLogo"
-     
+        component={Logo}
+        durationInFrames={150}
+        fps={30}
+        width={1920}
+        height={1080}
+        schema={myCompSchema2}
+        defaultProps={{
+          logoColor1: "#91dAE2" as const,
+          logoColor2: "#86A8E7" as const,
+        }}
+      />
+    </>
+  );
+};
